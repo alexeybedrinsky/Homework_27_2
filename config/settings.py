2cccc81d81951623856256075ef1d7986cce5c8e
alexeybedrinsky <bedrinsky@mail.ru>
2024-11-06 09:58:33 +0300

    внес изменения согласно рекомендациям наставника 1.установил psycopg2-binary 2.установил python-dotenv 3.обновил settings.py для .env и добавил AUTH_USER_MODEL = 'users.User' 4. Через pgAdmin 4 создал базу данных project_24 для работы проекта

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,17 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-SECRET_KEY = 'django-insecure-d(&$kolytg)04kj(b6vangui6h@6vz+jlc-rakd5un-49j^)cf'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
@@ -63,8 +66,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
@@ -109,3 +116,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
